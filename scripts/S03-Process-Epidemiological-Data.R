637f3b964b7ea46a30008e63c3f5c58324c817d6
lfrancois <liesbeth.francois@ucb.com>
2019-06-28 17:20:12 +0200

    Adding epidemiological data for Orphanet

---
+++ scripts/S03-Process-Epidemiological-Data.R
@@ -0,0 +1,113 @@
+##############################################################
+##############################################################
+## Epidemiological data
+##############################################################
+##############################################################
+
+library(here)
+library(BiocParallel)
+library(XML)
+library(tidyverse)
+
+# source(here("scripts/clinVar-Functions.R"))
+
+##
+bpparam <- MulticoreParam(30)
+sdir <- here("sources")
+ddir <- here("data")
+
+###############################################################################@
+## Data from ClinVarFullRelease.xml.gz ----
+###############################################################################@
+
+readOrpha <- function(file, n=-1L){
+  orRaw <- readLines(file, n=n)
+  encoding <- sub(
+    "\".*$",
+    "",
+    sub("^[<][?]xml .*encoding=\"", "", orRaw[1])
+  )
+  starts <- grep("<Disorder id=.*?>", orRaw)
+  ends <- grep("<[/]Disorder>", orRaw)
+  starts <- starts[1:length(ends)]
+  orList <- apply(
+    cbind(starts, ends), 1,
+    function(x) sub(
+      "[^>]*$",
+      "",
+      sub(
+        "^[^<]*",
+        "",
+        paste(orRaw[x[1]:x[2]], collapse="\n")
+      )
+    )
+  )
+  attr(orList, "encoding") <- encoding
+  return(orList)
+}
+
+###############################################################################@
+## _+ Loading and parsing XML ----
+message("Loading XML...")
+message(Sys.time())
+xmlFile <- file.path(sdir, "orphanet/Epidemiological data/Rare disease epidemiology/en_product9_prev.xml")
+orList <- readOrpha(xmlFile) #, n=100000) # total: >37210660
+encoding <- attr(orList, "encoding")
+message(Sys.time())
+message("... Done\n")
+
+message("Parsing XML file ...")
+message(Sys.time())
+orParse <- do.call(
+  rbind,
+  bplapply(orList,
+    function(or){
+      orpa <- xmlRoot(xmlParse(or, encoding = encoding))
+      orphaNb <- xmlValue(orpa[["OrphaNumber"]])
+      orphaName <- xmlValue(orpa[["Name"]])
+      disType <- xmlValue(orpa[["DisorderType"]][["Name"]])
+      # nbPrev <- as.integer(xmlAttrs(a[["PrevalenceList"]]))
+      prevList <- do.call(
+        rbind,
+        lapply(xmlChildren(orpa[["PrevalenceList"]]),
+               function(x){
+                 source <- xmlValue(x[["Source"]])
+                 prevalenceType <- xmlValue(x[["PrevalenceType"]][["Name"]])
+                 prevalenceQualification <- xmlValue(x[["PrevalenceQualification"]][["Name"]])
+                 prevalenceClass <- xmlValue(x[["PrevalenceClass"]][["Name"]])
+                 prevalenceGeo <- xmlValue(x[["PrevalenceGeographic"]][["Name"]])
+                 prevalenceVal <- xmlValue(x[["PrevalenceValidationStatus"]][["Name"]])
+                 valMoy <- xmlValue(x[["ValMoy"]])
+                 toRet <- tibble(prevalenceSource = source,
+                                 prevalenceType = prevalenceType,
+                                 prevalenceQualification = prevalenceQualification,
+                                 prevalenceGeographic = prevalenceGeo,
+                                 prevalenceClass = prevalenceClass,
+                                 prevalenceValidationStatus = prevalenceVal,
+                                 prevalenceValue = valMoy)
+                 return(toRet)
+          })
+      )
+      toRet <- mutate(prevList,
+                      id = orphaNb,
+                      DB = "ORPHA",
+                      label = orphaName,
+                      type = disType) 
+      return(toRet)
+      },
+      BPPARAM = bpparam)
+  )
+message(Sys.time())
+message("... Done \n")
+
+###############################################################################@
+## Writing file
+
+write.table(
+  orParse,
+  file= here("data","Orphanet_EpidemiologicalDiseaseInfo.txt"),
+  sep="\t",
+  row.names=FALSE, col.names=TRUE,
+  quote=TRUE,
+  qmethod = "double"
+)
